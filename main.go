6dbc83494c02702a9b2c00a22d7a8dec3b1f7e05
nade-harlow <omonadefranklyn@gmail.com>
2022-02-25 13:13:35 +0100

    db instances refactored

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	_"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
 	"github.com/nade-harlow/e-library/app/controllers"
 	"github.com/nade-harlow/e-library/app/models"
 	"log"
@@ -12,9 +12,10 @@ import (
 func main() {
 	router := gin.Default()
 	db := models.Init()
-	di:= models.DbInstance{Postgres: db}
+	//di:= models.DbInstance{Postgres: db}
+	dbIns := models.NewInstance(db)
 	//c := controllers.NewHttp{&di}
-	Nh := controllers.New(&di)
+	Nh := controllers.New(dbIns)
 	Nh.Routes(router)
 	port := os.Getenv("DB_HOST")
 	log.Printf("Server listening on port %s\n", port)
