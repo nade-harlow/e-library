30f944923ede1eb5eda04612f30bd1a652d3c86b
nade-harlow <omonadefranklyn@gmail.com>
2022-02-25 13:13:02 +0100

    db queries refactored

--- app/models/db.go
+++ app/models/db.go
@@ -1,6 +1,9 @@
 package models
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 type Db interface {
 	Create(book Book) error
@@ -8,7 +11,7 @@ type Db interface {
 }
 
 func (db *DbInstance) Create(book Book) error {
-	stmt, err := db.Postgres.Prepare("INSERT INTO book (id, title, author, available, created_at, modified_at) VALUES ($1, $2, $3, $4, $5, $6)")
+	stmt, err := db.Postgres.Prepare(fmt.Sprintf("INSERT INTO book (id, title, author, available, created_at, modified_at) VALUES ($1, $2, $3, $4, $5, $6)"))
 	if err != nil {
 		return err
 	}
@@ -20,13 +23,13 @@ func (db *DbInstance) Create(book Book) error {
 }
 
 func (db *DbInstance) AllBooks() ([]Book, error) {
-	books:= []Book{}
-	row, err := db.Postgres.Query("SELECT * FROM book")
+	books := []Book{}
+	row, err := db.Postgres.Query(fmt.Sprintf("SELECT * FROM book"))
 	if err != nil {
 		return nil, err
 	}
-	for row.Next(){
-		book:= Book{}
+	for row.Next() {
+		book := Book{}
 		err = row.Scan(&book.ID, &book.Title, &book.Author, &book.Available, &book.CreatedAt, &book.ModifiedAt)
 		if err != nil {
 			return nil, err
@@ -37,8 +40,8 @@ func (db *DbInstance) AllBooks() ([]Book, error) {
 }
 
 func (db DbInstance) CheckBookAvailability(id string) bool {
-	book:= Book{}
-	row := db.Postgres.QueryRow("SELECT available FROM book WHERE id = ", id)
+	book := Book{}
+	row := db.Postgres.QueryRow(fmt.Sprintf("SELECT available FROM book WHERE id = "), id)
 	err := row.Scan(&book)
 	if err != nil {
 		log.Println("")
