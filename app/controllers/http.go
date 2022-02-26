eb8d0cfecca9ee016d199d5aa6253dd67da5b763
nade-harlow <omonadefranklyn@gmail.com>
2022-02-26 23:03:04 +0100

    borrow book handler created

--- app/controllers/http.go
+++ app/controllers/http.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/nade-harlow/e-library/app/models"
@@ -29,7 +28,6 @@ func (h *NewHttp) AddBook() gin.HandlerFunc {
 		book.ID = uuid.NewString()
 		book.CreatedAt = time.Now().String()
 		book.ModifiedAt = time.Now().String()
-		fmt.Println(book.ID)
 		err = h.Db.Create(book)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -50,7 +48,7 @@ func (h *NewHttp) GetAllBooks() gin.HandlerFunc {
 	}
 }
 
-func (h NewHttp) CheckIn() gin.HandlerFunc {
+func (h *NewHttp) CheckIn() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		student := models.Student{}
 		err := c.ShouldBindJSON(&student)
@@ -64,6 +62,26 @@ func (h NewHttp) CheckIn() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+		c.JSON(200, gin.H{"response": "Successfully checked in. Happy reading"})
 		c.Set("student", student.ID)
+	}
+}
+
+func (h NewHttp) BorrowBook() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		student, exist := c.Get("student")
+		if !exist {
+			c.JSON(http.StatusNotFound, gin.H{"error": "sorry, you have to check in first"})
+		}
+		studentID := student.(*models.Student).ID
+		book, err := h.Db.GetBookByTitle("Things fall apart")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		err = h.Db.BorrowBook(book.ID, studentID)
+		if err != nil {
+			return
+		}
 	}
 }
