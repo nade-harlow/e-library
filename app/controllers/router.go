d101393246e8dd51ce6c1b1f58650a8dd40d9e94
nade-harlow <omonadefranklyn@gmail.com>
2022-02-27 00:35:37 +0100

    routes refactored

--- app/controllers/router.go
+++ app/controllers/router.go
@@ -2,13 +2,10 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
-	"os"
 )
 
 func (h *NewHttp) Routes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
-		log.Println(os.Getenv("DB_PORT"))
 		c.JSON(200, gin.H{"message": "hello"})
 	})
 	r.POST("/add", h.AddBook())
