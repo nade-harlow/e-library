03b83488832940b56eed6822aee4d55946f7c8ca
nade-harlow <omonadefranklyn@gmail.com>
2022-02-27 14:56:33 +0100

    refactored routes into groups

--- app/controllers/router.go
+++ app/controllers/router.go
@@ -2,17 +2,33 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/e-library/app/middleware"
 )
 
 func (h *NewHttp) Routes(r *gin.Engine) {
+	r.Use(middleware.Session())
+	r.Use(gin.Logger())
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "hello"})
 	})
-	r.POST("/add", h.AddBook())
-	r.GET("/get-all-books", h.GetAllBooks())
-	r.POST("/check-in", h.CheckIn())
-	r.POST("/borrow/:student-id/:book-title", h.BorrowBook())
-	r.POST("/return-book/:student-id/:book-title", h.ReturnBook())
-	r.GET("/lenders", h.GetAllBorrowedBooks())
-	r.PUT("/update/:book-title/:status", h.UpdateBookStatus())
+
+	book := r.Group("library/book")
+	{
+		book.POST("/add-book", h.AddBook())
+		book.GET("/get-all-books", h.GetAllBooks())
+		book.PUT("/update/:book-title/:status", h.UpdateBookStatus())
+	}
+
+	student := r.Group("library/student")
+	{
+		student.GET("/borrow/:book-title", h.BorrowBook())
+		student.POST("/return-book/:student-id/:book-title", h.ReturnBook())
+		student.POST("/check-in", h.CheckIn())
+	}
+
+	lend := r.Group("library/lend")
+	{
+		lend.GET("/get-lenders", h.GetAllBorrowedBooks())
+	}
+
 }
