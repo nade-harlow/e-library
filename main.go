3d80fa01363267e45f394a853fb726dd5382e2dd
nade-harlow <omonadefranklyn@gmail.com>
2022-02-28 18:08:44 +0100

    comments cleaned out

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ import (
 func main() {
 	router := gin.Default()
 	db := models.Init()
-	//di:= models.DbInstance{Postgres: db}
 	dbIns := models.NewInstance(db)
-	//c := controllers.NewHttp{&di}
 	Nh := controllers.New(dbIns)
 	Nh.Routes(router)
 	port := os.Getenv("DB_HOST")
