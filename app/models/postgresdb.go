96fdb3bcd89f432eecb72b5cd14e5986814c0677
nade-harlow <omonadefranklyn@gmail.com>
2022-03-18 00:08:23 +0100

    postres url added

--- app/models/postgresdb.go
+++ app/models/postgresdb.go
@@ -2,11 +2,9 @@ package models
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
-	"os"
 )
 
 type DbInstance struct {
@@ -27,9 +25,9 @@ func NewInstance(db *sql.DB) *DbInstance {
 }
 
 func postgresql() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	db, err := sql.Open("postgres", psqlInfo)
+	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	//	os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	db, err := sql.Open("postgres", "postgres://evrhmwwrktikrl:6f6ca098e07d85aa7250a7397d06f79dc0d06e120b99a1c1d7d8ac2432118067@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/dfemrlacff0t1")
 	if err != nil {
 		log.Fatalf("failed to open database %s", err.Error())
 	}
