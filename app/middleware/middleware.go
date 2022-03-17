fb3198d22d98a83510999b2f0e16ed60915a9fd8
nade-harlow <omonadefranklyn@gmail.com>
2022-03-17 22:06:33 +0100

    persisted student id in cookies

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -2,12 +2,18 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/nade-harlow/e-library/app/helper"
+	"net/http"
 )
 
 func Session() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set("student", helper.Session())
+		cookie, err := c.Cookie("session")
+		if err != nil {
+			c.Redirect(http.StatusFound, "/")
+			return
+		}
+
+		c.Set("student", cookie)
 		c.Next()
 	}
 }
