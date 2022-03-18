ca4e269ea34ba44414addf8c193f44836236a977
nade-harlow <omonadefranklyn@gmail.com>
2022-03-18 19:36:19 +0100

    db

--- app/models/postgresdb.go
+++ app/models/postgresdb.go
@@ -2,12 +2,10 @@ package models
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
-	"os"
-
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 type DbInstance struct {
@@ -28,21 +26,9 @@ func NewInstance(db *sql.DB) *DbInstance {
 }
 
 func postgresql() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
-	if os.Getenv("DB_USER") == "" {
-		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-		if err != nil {
-			log.Fatalf("failed to open database %s", err.Error())
-		}
-		err = db.Ping()
-		if err != nil {
-			log.Fatalf("failed to verify database connection %s", err.Error())
-		}
-		log.Println("Successfully connected!")
-		return db
-	}
-	db, err := sql.Open("postgres", psqlInfo)
+	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	//	os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("failed to open database %s", err.Error())
 	}
