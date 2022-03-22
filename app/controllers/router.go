cb25f43c585a47c854025fe3c0674121985ccb04
nade-harlow <omonadefranklyn@gmail.com>
2022-03-22 09:43:47 +0100

    lend routes refactored

--- app/controllers/router.go
+++ app/controllers/router.go
@@ -41,4 +41,8 @@ func (h *NewHttp) Routes(r *gin.Engine) {
 		student.POST("/check-in", h.CheckIn())
 	}
 
+	lend := r.Group("library/lend", middleware.Session())
+	{
+		lend.GET("/get-lenders", h.GetAllBorrowedBooks())
+	}
 }
