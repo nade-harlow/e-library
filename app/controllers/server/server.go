479f03d1e29c2c1f664bdfc7cad26e360a7015cc
nade-harlow <omonadefranklyn@gmail.com>
2022-03-24 13:56:44 +0100

    server file created

---
+++ app/controllers/server/server.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nade-harlow/e-library/app/controllers"
+	"github.com/nade-harlow/e-library/app/models"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Start() {
+	router := gin.Default()
+	router.StaticFS("static", http.Dir("app/views/static"))
+	router.LoadHTMLGlob("app/views/html/*")
+	db := models.Init()
+	dbInstance := models.NewInstance(db)
+	Newhttp := controllers.New(dbInstance)
+	Newhttp.Routes(router)
+	port := ":" + os.Getenv("PORT")
+	if port == ":" {
+		port += "8080"
+	}
+	log.Printf("Server listening on port %s\n", port)
+	err := router.Run(port)
+	if err != nil {
+		log.Fatalf("server failed to listen on port %s", port)
+	}
+}
